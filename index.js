220aef4a972af1b25d0d017d606b2d36aae28650
Gavin Sharkey <gavinsharkey2016@gmail.com>
2021-01-31 19:02:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0]);
+  if (string.length > 1) {
+    printString(string.substring(1, string.length))
+  }
+}
+
+function reverseString(string) {
+  if (string.length === 1) {
+    return string;
+  } else {
+    return string[string.length - 1] + reverseString(string.substring(0, string.length - 1))
+  }
+}
+
+reverseString('Hello')
