7e5b5b5886f6dc5f4da12d318dfcd818bf5bd865
Gavin Sharkey <gavinsharkey2016@gmail.com>
2021-01-31 19:10:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function reverseString(string) {
   if (string.length === 1) {
     return string;
   } else {
-    return string[string.length - 1] + reverseString(string.substring(0, string.length - 1))
+    return string[string.length - 1] + reverseString(string.substring(0, string.length - 1));
   }
 }
 
@@ -21,12 +21,12 @@ function isPalindrome(string) {
   } 
   
   if (string[0] === string[string.length - 1]) {
-    return isPalindrome(string.substring(1, string.length - 1))
+    return isPalindrome(string.substring(1, string.length - 1));
   }
   
   return false;
 }
 
-console.log(isPalindrome('i'))
+
 
 
